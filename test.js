ab481dabc273c4641ce1e721457c1431ae23fe62
dzhakhar <dzhakhar.ukhaev@gmail.com>
2016-06-17 05:21:37 +0600

    initial commit

---
+++ test.js
@@ -0,0 +1,32 @@
+var Reactive = function(){
+	this.state = {};
+	this.newValue = function(key, value){
+		this.state[key] = {};
+		this.state[key]["value"] = value;
+		this.state[key]["listeners"] = [];
+	}
+
+	this.addListener = function(key, func, callback){
+		this.state[key]["listeners"].push(func);
+		if(callback){
+			callback();
+		}
+	}
+
+	this.changeValue = function(key, value){
+		this.state[key]["value"] = value;
+		var listeners = this.state[key]["listeners"];
+		for(var i = 0; i < listeners.length; i++){
+			listeners[i](value);
+		}
+	}
+}
+
+var test = new Reactive();
+var foo = function(val){console.log("Hello " + val)};
+
+test.newValue("name", "Vasya");
+test.addListener("name", foo);
+test.changeValue("name", "Petya");
+test.changeValue("name", "Alesha");
+test.changeValue("name", "Fedya");
